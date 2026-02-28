ebc8c1409c2c5fa73d6cb890ac08811855b14224
Gesugao-san <gesugao.san@gmail.com>
2026-02-28 23:11:45 +0300

    Update captive_portal_test_urls.bat

--- windows/experiments/captive_portal_test_urls.bat
+++ windows/experiments/captive_portal_test_urls.bat
@@ -1,6 +1,7 @@
 @echo off
 chcp 65001 >nul
 setlocal enabledelayedexpansion
+cls
 
 echo ===================================================
 echo     Checking captive portal endpoints via curl
@@ -19,9 +20,6 @@ if %errorlevel% neq 0 (
     where curl
     echo.
 )
-
-:: Clear screen (CLS)
-cls
 
 :: Google HTTPS (main)
 set urls[0]=https://www.google.com/generate_204
@@ -123,13 +121,160 @@ set urls[40]=https://elementary.io/generate_204
 :: HTTP status service
 set urls[41]=http://httpstat.us/204
 
-:: Yandex
-set urls[42]=http://api.browser.yandex.ru/generate_204
+:: OS/Device Specific
+:: Ubuntu/Linux (NetworkManager)
+set urls[42]=http://connectivity-check.ubuntu.com/
+:: Samsung
+set urls[43]=http://connectivitycheck.samsung.com/generate_204
+:: Huawei/Honor (alternative)
+set urls[44]=http://connectivitycheck.platform.hicloud.com/generate_204
+:: Amazon FireOS
+set urls[45]=http://device.amazon.com/generate_204
+:: Apple (additional)
+set urls[46]=http://gsp1.apple.com/
+set urls[47]=http://www.appleiphonecell.com/
+set urls[48]=http://www.itools.info/
+
+:: Microsoft (additional)
+:: Microsoft IPv6
+set urls[49]=http://ipv6.msftconnecttest.com/connecttest.txt
+:: Microsoft Edge alternative
+set urls[50]=http://edge-http.microsoft.com/generate_204
+:: Microsoft 365 connectivity test
+set urls[51]=https://connectivity.m365.cloud.microsoft/
+
+:: Cloudflare & DNS Providers
+:: Cloudflare additional endpoints
+set urls[52]=http://cp.cloudflare.com/generate_204
+set urls[53]=https://1.1.1.1/cdn-cgi/trace
+set urls[54]=http://www.cloudflare.com/generate_204
+:: Google DNS
+set urls[55]=https://dns.google/resolve?name=example.com&type=A
+:: OpenDNS/Cisco
+set urls[56]=http://www.opendns.com/
+:: Quad9 DNS
+set urls[57]=https://www.quad9.net/
+
+:: Social Media & Services
+:: Meta (Facebook)
+set urls[58]=https://www.facebook.com/
+set urls[59]=https://m.me/
+set urls[60]=https://www.whatsapp.com/
+:: Instagram
+set urls[61]=https://www.instagram.com/
+:: X (Twitter)
+set urls[62]=https://twitter.com/
+:: LinkedIn
+set urls[63]=https://www.linkedin.com/
+:: Pinterest
+set urls[64]=https://www.pinterest.com/
+:: Snapchat
+set urls[65]=https://www.snapchat.com/
+:: Reddit
+set urls[66]=https://www.reddit.com/
+:: Tumblr
+set urls[67]=https://www.tumblr.com/
+:: Threads
+set urls[68]=https://www.threads.net/
+:: Bluesky
+set urls[69]=https://bsky.app/
+:: Twitch
+set urls[70]=https://www.twitch.tv/
+:: TikTok
+set urls[71]=https://www.tiktok.com/
+:: Signal
+set urls[72]=https://signal.org/
+:: Telegram Web
+set urls[73]=https://web.telegram.org/
+
+:: Search Engines & Portals
+set urls[74]=https://duckduckgo.com/
+set urls[75]=https://www.bing.com/
+set urls[76]=https://www.yahoo.com/
+set urls[77]=https://www.baidu.com/
+
+:: Video & Streaming
+set urls[78]=https://youtube.com/shorts/
+set urls[79]=https://vimeo.com/
+set urls[80]=https://www.dailymotion.com/
+set urls[81]=https://www.netflix.com/
+set urls[82]=https://open.spotify.com/
+set urls[83]=https://soundcloud.com/
+
+:: E-commerce & Marketplaces
+set urls[84]=https://www.amazon.com/
+set urls[85]=https://www.amazon.de/
+set urls[86]=https://www.amazon.co.uk/
+set urls[87]=https://www.ebay.com/
+set urls[88]=https://www.etsy.com/
+set urls[89]=https://www.shopify.com/
+set urls[90]=http://www.aliexpress.com/
+set urls[91]=https://www.alibaba.com/
+set urls[92]=https://www.temu.com/
+set urls[93]=https://ru.shein.com/
+
+:: Tech & AI Companies
+set urls[94]=https://www.microsoft.com/
+set urls[95]=https://www.apple.com/
+set urls[96]=https://openai.com/
+set urls[97]=https://www.anthropic.com/
+set urls[98]=https://x.ai/
+set urls[99]=https://www.deepseek.com/
+set urls[100]=https://ai.google/
+set urls[101]=https://ai.meta.com/
+set urls[102]=https://www.perplexity.ai/
+set urls[103]=https://huggingface.co/
+set urls[104]=https://github.com/
+set urls[105]=https://about.gitlab.com/
+set urls[106]=https://stackoverflow.com/
+
+:: Cloud Services & Hosting
+set urls[107]=https://cloud.google.com/
+set urls[108]=https://azure.microsoft.com/
+set urls[109]=https://www.digitalocean.com/
+set urls[110]=https://vercel.com/
+set urls[111]=https://www.netlify.com/
+set urls[112]=https://www.heroku.com/
+
+:: Education & Science
+set urls[113]=https://www.wikipedia.org/
+set urls[114]=https://www.coursera.org/
+set urls[115]=https://www.edx.org/
+set urls[116]=https://www.khanacademy.org/
+set urls[117]=https://www.duolingo.com/
+set urls[118]=https://arxiv.org/
+set urls[119]=https://scholar.google.com/
+set urls[120]=https://www.researchgate.net/
+
+:: Crypto & Web3
+set urls[121]=https://www.coinbase.com/
+set urls[122]=https://www.binance.com/
+set urls[123]=https://ethereum.org/
+set urls[124]=https://opensea.io/
+
+:: Chinese services
+set urls[125]=https://www.wechat.com/
+set urls[126]=https://www.baidu.com/
+set urls[127]=http://www.aliexpress.com/
+set urls[128]=https://www.alibaba.com/
+
+:: Russian services
+set urls[129]=https://vk.com/
+set urls[130]=https://rutube.ru/
+set urls[131]=https://www.wildberries.ru/
+set urls[132]=https://www.ozon.ru/
+set urls[133]=https://www.avito.ru/
+set urls[134]=https://yandex.ru/
+set urls[135]=https://mail.ru/
+set urls[136]=https://www.rambler.ru/
+
+:: Yandex (captive portal endpoint)
+set urls[137]=http://api.browser.yandex.ru/generate_204
 
 echo Checking the following endpoints:
 echo.
-:: Loop through all possible indices (0 to 42)
-for /l %%i in (0,1,42) do (
+:: Loop through all possible indices (0 to 137)
+for /l %%i in (0,1,137) do (
     if defined urls[%%i] (
         echo   [!urls[%%i]!]
     )
@@ -138,8 +283,8 @@ echo.
 echo ===================================================
 echo.
 
-:: Loop through all possible indices (0 to 42)
-for /l %%i in (0,1,42) do (
+:: Loop through all possible indices (0 to 137)
+for /l %%i in (0,1,137) do (
     if defined urls[%%i] (
         set "current_url=!urls[%%i]!"
         
